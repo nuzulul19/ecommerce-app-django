cb2b6f56dd9edda6136e4272d4acc230f27f0394
Nuzulul Aulia <nuzulul19@gmail.com>
2022-12-25 19:54:00 +0700

    Build User Registration

--- ecommerce_app_django/settings.py
+++ ecommerce_app_django/settings.py
@@ -40,9 +40,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Django App
+    "account",
     "cart",
     "store",
+    # 3rd parties
+    "crispy_forms",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- ecommerce_app_django/urls.py
+++ ecommerce_app_django/urls.py
@@ -20,9 +20,10 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("cart/", include("cart.urls")),
     path("", include("store.urls")),
+    path("admin/", admin.site.urls),
+    path("account/", include("account.urls")),
+    path("cart/", include("cart.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
